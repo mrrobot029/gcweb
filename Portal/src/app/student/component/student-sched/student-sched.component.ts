0bec803fa3a0f260823eaf99ca6d8cacb9742c7e
Mr. Robot <lorenzo.medenilla@gmail.com>
2020-01-20 08:55:40 +0800

    prospectus ok na

--- Portal/src/app/student/component/student-sched/student-sched.component.ts
+++ Portal/src/app/student/component/student-sched/student-sched.component.ts
@@ -17,6 +17,7 @@ export class StudentSchedComponent implements OnInit {
   ngOnInit() {
     this.credStud = JSON.parse(localStorage.getItem('gcweb_student'));
     this.classId['si_idnumber'] = this.credStud.data[0].si_idnumber
+    this.classId['hatdog'] = 'hatdogs'
     this.ds.sendRequest('getStudentSchedule', this.classId).subscribe((res) => {
       this.classStud = res;
       console.log(this.classStud);

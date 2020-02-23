7b82813841dc79ebce861b67d91753f5402d25af
ianstrong <34215734+ianstrong@users.noreply.github.com>
2020-02-23 09:30:02 +0800

    deleted unnecessary files

--- gcat/src/app/admin/admin.module.ts
+++ gcat/src/app/admin/admin.module.ts
@@ -4,23 +4,17 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DataService } from '../services/data.service';
 import { FormsModule } from '@angular/forms';
-import { AdminPage1Component } from './component/admin-page1/admin-page1.component';
-import { AdminPage2Component } from './component/admin-page2/admin-page2.component';
-import { AdminPage3Component } from './component/admin-page3/admin-page3.component';
 import { UsersComponent } from './component/users/users.component';
 import { ApplicantsComponent } from './component/applicants/applicants.component';
-import {NgxPaginationModule} from 'ngx-pagination';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { ScheduledComponent } from './component/scheduled/scheduled.component';
 import { NgxSpinnerModule } from "ngx-spinner";
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ConfirmedapplicantsComponent } from './component/confirmedapplicants/confirmedapplicants.component';
 
 
 @NgModule({
   declarations: [
-    AdminPage1Component,
-    AdminPage2Component,
-    AdminPage3Component,
     UsersComponent,
     ApplicantsComponent,
     ScheduledComponent,

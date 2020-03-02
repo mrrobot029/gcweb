2eed911e033f1404b2f8efdd2c12ec88691df9a8
Mr. Robot <lorenzo.medenilla@gmail.com>
2020-03-02 11:08:14 +0800

    asd

--- gcat/src/app/admin/admin-routing.module.ts
+++ gcat/src/app/admin/admin-routing.module.ts
@@ -1,29 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ApplicantsComponent } from './component/applicants/applicants.component';
-import { UsersComponent } from './component/users/users.component';
-import { ScheduledComponent } from './component/scheduled/scheduled.component'
-import { ConfirmedapplicantsComponent } from './component/confirmedapplicants/confirmedapplicants.component'
-import { AuthGuard } from '../services/auth.guard';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ApplicantsComponent } from "./component/applicants/applicants.component";
+import { UsersComponent } from "./component/users/users.component";
+import { ScheduledComponent } from "./component/scheduled/scheduled.component";
+import { ConfirmedapplicantsComponent } from "./component/confirmedapplicants/confirmedapplicants.component";
+import { AuthGuard } from "../services/auth.guard";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'applicants', pathMatch: 'full' },
+  { path: "", redirectTo: "applicants", pathMatch: "full" },
   {
-    path: 'users', component: UsersComponent,
-    // canActivate: [AuthGuard] 
+    path: "users",
+    component: UsersComponent,
+    canActivate: [AuthGuard]
   },
   {
-    path: 'applicants', component: ApplicantsComponent,
-    //  canActivate: [AuthGuard] 
+    path: "applicants",
+    component: ApplicantsComponent,
+    canActivate: [AuthGuard]
   },
   {
-    path: 'unscheduled', component: ConfirmedapplicantsComponent,
-    //  canActivate: [AuthGuard] 
+    path: "unscheduled",
+    component: ConfirmedapplicantsComponent,
+    canActivate: [AuthGuard]
   },
   {
-    path: 'scheduled', component: ScheduledComponent,
-    // canActivate: [AuthGuard] 
+    path: "scheduled",
+    component: ScheduledComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
@@ -31,5 +34,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
